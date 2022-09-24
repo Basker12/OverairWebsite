e960a0cef31a32f6c2c379ca9a384e507bff8ee5
kacper <kacperileon@gmail.com>
2022-09-24 18:02:47 +0100

    [Addition] Finally fixed the animations for when a user opens the cardbox +

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-let counterContainer = document.querySelector('#counterSection');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
 
-function counter(id, start, end, duration) {
-    let obj = document.getElementById(id),
-        current = start,
-        range = end - start,
-        increment = end > start ? 1 : -1,
-        step = Math.abs(Math.floor(duration / range)),
-        timer = setInterval(() => {
-            current += increment;
-            obj.textContent = current;
-            if (current === end) {
-                clearInterval(timer);
-            }
-        }, step);
-}
-counter("count1", 0, 1000, 3000);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showHidden');
+        } else {
+            entry.target.classList.remove('showHidden');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", function() {
+    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.srcset = lazyImage.dataset.srcset;
+                    lazyImage.classList.remove("lazy");
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        // Possibly fall back to event handlers here
+    }
+});
---
+++ sideScript.js
@@ -0,0 +1,246 @@
+/* ------------------------------------------------------------------ */
+const repairBox = document.querySelector('#openRepair');
+const closeRepair = document.querySelector('#closeRepair');
+
+repairBox.addEventListener('click', () => {
+    document.querySelector('#mySidenav').style.width = "100%";
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+
+        let vis = document.querySelectorAll('.notVisible');
+        if (vis.length === 0) {
+            return null;
+        } else {
+            const hiddenElement = document.querySelectorAll('.notVisible');
+            hiddenElement.forEach(el => el.classList.add('visible'));
+        }
+
+    }, delayAnim);
+/*
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current === end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 4000, 250);
+*/
+});
+
+closeRepair.addEventListener('click', () => {
+    /*
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.replace('visible','notVisible');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.visible');
+    hiddenElements.forEach((el) => observer.observe(el));
+*/
+    let vis = document.querySelectorAll('.visible');
+    if (vis.length === 0) {
+        return null;
+    } else {
+        const hiddenElement = document.querySelectorAll('.visible');
+        hiddenElement.forEach(el => el.classList.replace('visible','notVisible'));
+    }
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+        document.querySelector('#mySidenav').style.width = "0%";
+    }, delayAnim);
+});
+/* ------------------------------------------------------------------ */
+
+
+/* ------------------------------------------------------------------ */
+
+const upgradeBox = document.querySelector('#openUpgrade');
+const closeUpgrade = document.querySelector('#closeUpgrade');
+
+upgradeBox.addEventListener('click', () => {
+    document.querySelector('#myUpgrade').style.width = "100%";
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+
+        let vis = document.querySelectorAll('.notVisible');
+        if (vis.length === 0) {
+            return null;
+        } else {
+            const hiddenElement = document.querySelectorAll('.notVisible');
+            hiddenElement.forEach(el => el.classList.add('visible'));
+        }
+
+    }, delayAnim);
+/*
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current === end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count2", 0, 2000, 250);
+ */
+});
+
+closeUpgrade.addEventListener('click', () => {
+    let vis = document.querySelectorAll('.visible');
+    if (vis.length === 0) {
+        return null;
+    } else {
+        const hiddenElement = document.querySelectorAll('.visible');
+        hiddenElement.forEach(el => el.classList.replace('visible','notVisible'));
+    }
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+        document.querySelector('#myUpgrade').style.width = "0%";
+    }, delayAnim);
+});
+/* ------------------------------------------------------------------ */
+
+
+/* ------------------------------------------------------------------ */
+
+const recoverBox = document.querySelector('#openRecover');
+const closeRecover = document.querySelector('#closeRecover');
+recoverBox.addEventListener('click', () => {
+    document.querySelector('#myRecover').style.width = "100%";
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+
+        let vis = document.querySelectorAll('.notVisible');
+        if (vis.length === 0) {
+            return null;
+        } else {
+            const hiddenElement = document.querySelectorAll('.notVisible');
+            hiddenElement.forEach(el => el.classList.add('visible'));
+        }
+
+    }, delayAnim);
+
+/*
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current === end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count3", 0, 500, 250);
+
+ */
+});
+
+closeRecover.addEventListener('click', () => {
+    let vis = document.querySelectorAll('.visible');
+    if (vis.length === 0) {
+        return null;
+    } else {
+        const hiddenElement = document.querySelectorAll('.visible');
+        hiddenElement.forEach(el => el.classList.replace('visible','notVisible'));
+    }
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+        document.querySelector('#myRecover').style.width = "0%";
+    }, delayAnim);
+});
+/* ------------------------------------------------------------------ */
+
+
+/* ------------------------------------------------------------------ */
+const purchaseBox = document.querySelector('#openPurchase');
+const closePurchase = document.querySelector('#closePurchase');
+
+purchaseBox.addEventListener('click', () => {
+    document.querySelector('#myPurchase').style.width = "100%";
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+
+        let vis = document.querySelectorAll('.notVisible');
+        if (vis.length === 0) {
+            return null;
+        } else {
+            const hiddenElement = document.querySelectorAll('.notVisible');
+            hiddenElement.forEach(el => el.classList.add('visible'));
+        }
+
+    }, delayAnim);
+
+
+/*
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current === end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count4", 0, 40, 2000);
+
+ */
+});
+
+closePurchase.addEventListener('click', () => {
+    let vis = document.querySelectorAll('.visible');
+    if (vis.length === 0) {
+        return null;
+    } else {
+        const hiddenElement = document.querySelectorAll('.visible');
+        hiddenElement.forEach(el => el.classList.replace('visible','notVisible'));
+    }
+
+    let delayAnim = 1000;
+
+    setTimeout(function () {
+        document.querySelector('#myPurchase').style.width = "0%";
+    }, delayAnim);
+
+});
+/* ------------------------------------------------------------------ */
