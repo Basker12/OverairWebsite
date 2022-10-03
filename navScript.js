45e9d6c2a9fd1e2956f1029917574c735b20a14c
kacper <kacperileon@gmail.com>
2022-10-03 15:19:08 +0100

    [Addition] Added lots of meta tags for better SEO.

--- navScript.js
+++ navScript.js
@@ -1,15 +1,1 @@
-let navbar = document.querySelector('nav');
-let sticky = navbar.offsetTop;
-
-
-window.onscroll = function() {
-    stickyNav()
-};
-
-function stickyNav() {
-    if (window.scrollY >= sticky) {
-        navbar.classList.add('sticky');
-    } else {
-        navbar.classList.remove('sticky');
-    }
-}
+let navbar=document.querySelector("nav"),sticky=navbar.offsetTop;function stickyNav(){window.scrollY>=sticky?navbar.classList.add("sticky"):navbar.classList.remove("sticky")}window.onscroll=function(){stickyNav()};
